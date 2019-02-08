327d2dcec4eb0961328a5cd004eab5cdee057cbb
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-08 14:10:03 +0700

    commit

---
+++ pypi/py-pandoc-crossref/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+__version__ = '0.3.4.0'
+
+setup(
+    name='py-pandoc-crossref',
+    version=__version__,
+
+    description='pandoc-crossref in pip and conda',
+    url='https://github.com/kiwi0fruit/py-pandoc-crossref',
+    author='kiwi0fruit',
+    author_email='peter.zagubisalo@gmail.com',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
