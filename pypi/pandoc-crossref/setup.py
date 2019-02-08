286d99442e1a589ef63d82213cee0a08c00ec703
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-08 12:47:22 +0700

    commit

--- pypi/pandoc-crossref/setup.py
+++ pypi/pandoc-crossref/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='pandoc-crossref',
-    version='0.1',
+    version='0.1.1',
 
-    description='Stub that prevents name clash with https://github.com/conda-forge/pandoc-crossref-feedstock For pandoc-crossref see https://github.com/kiwi0fruit/pypandoc-crossref',
-    url='https://github.com/kiwi0fruit/pypi-pandoc-crossref',
+    description='Stub that prevents name clash with https://github.com/conda-forge/pandoc-crossref-feedstock For pandoc-crossref see https://github.com/kiwi0fruit/py-pandoc-crossref',
+    url='https://github.com/kiwi0fruit/py-pandoc-crossref',
     author='kiwi0fruit',
     author_email='peter.zagubisalo@gmail.com',
     license='MIT',
